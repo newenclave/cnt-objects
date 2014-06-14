5be48465e533288e1d67779c12541b51e23b325a
newenclave <pikratacid@gmail.com>
2014-06-14 12:50:31 +0400

    cnt

--- lib/src/cnt-memblock.c
+++ lib/src/cnt-memblock.c
@@ -10,7 +10,7 @@ static unsigned int hash( const struct CntObject *obj );
 
 #define cnt_this_object_type_id CNT_OBJ_MEMBLOCK
 
-#define MBIMPL_PTR( mb ) (mb)->impl_
+#define MBPIMPL( mb ) (mb)->impl_
 
 #ifdef _MSC_VER
 
@@ -86,79 +86,79 @@ CntMemblock *cnt_memblock_new_reserved( size_t reserve_size )
 size_t cnt_memblock_size( const CntMemblock *mb )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_size( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_size( MBPIMPL( mb ) );
 }
 
 size_t cnt_memblock_capacity( const CntMemblock *mb )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_capacity( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_capacity( MBPIMPL( mb ) );
 }
 
 size_t cnt_memblock_available( const CntMemblock *mb )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_available( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_available( MBPIMPL( mb ) );
 }
 
 void  *cnt_memblock_begin( CntMemblock *mb )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_begin( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_begin( MBPIMPL( mb ) );
 }
 
 void  *cnt_memblock_end( CntMemblock *mb )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_end( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_end( MBPIMPL( mb ) );
 }
 
 void  *cnt_memblock_at( CntMemblock *mb, size_t position )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_at( MBIMPL_PTR( mb ), position );
+    return cnt_memblock_impl_at( MBPIMPL( mb ), position );
 }
 
 const void  *cnt_memblock_const_begin(const CntMemblock *mb)
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_const_begin( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_const_begin( MBPIMPL( mb ) );
 }
 
 const void  *cnt_memblock_const_end(const CntMemblock *mb)
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_const_end( MBIMPL_PTR( mb ) );
+    return cnt_memblock_impl_const_end( MBPIMPL( mb ) );
 }
 
 const void  *cnt_memblock_const_at(const CntMemblock *mb, size_t position)
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_const_at( MBIMPL_PTR( mb ), position);
+    return cnt_memblock_impl_const_at( MBPIMPL( mb ), position);
 }
 
 int cnt_memblock_reserve ( CntMemblock *mb, size_t new_size )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_reserve ( MBIMPL_PTR( mb ), new_size );
+    return cnt_memblock_impl_reserve ( MBPIMPL( mb ), new_size );
 }
 
 int cnt_memblock_resize(CntMemblock *mb, size_t new_size)
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_resize( MBIMPL_PTR( mb ), new_size);
+    return cnt_memblock_impl_resize( MBPIMPL( mb ), new_size);
 }
 
 int cnt_memblock_push_back( CntMemblock *mb, char c)
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_push_back( MBIMPL_PTR( mb ), c );
+    return cnt_memblock_impl_push_back( MBPIMPL( mb ), c );
 }
 
 void cnt_memblock_clear( CntMemblock *mb )
 {
     assert( mb != NULL );
-    cnt_memblock_impl_clear( MBIMPL_PTR( mb ) );
+    cnt_memblock_impl_clear( MBPIMPL( mb ) );
 }
 
 void cnt_memblock_swap ( CntMemblock *lmb, CntMemblock *rmb )
@@ -177,13 +177,13 @@ void cnt_memblock_swap ( CntMemblock *lmb, CntMemblock *rmb )
 void cnt_memblock_zero( CntMemblock *mb )
 {
     assert( mb != NULL );
-    cnt_memblock_impl_zero( MBIMPL_PTR( mb ) );
+    cnt_memblock_impl_zero( MBPIMPL( mb ) );
 }
 
 void *cnt_memblock_create_back( CntMemblock *mb, size_t count )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_create_back( MBIMPL_PTR( mb ), count );
+    return cnt_memblock_impl_create_back( MBPIMPL( mb ), count );
 }
 
 void *cnt_memblock_create_front( CntMemblock *mb, size_t count )
@@ -195,32 +195,32 @@ void *cnt_memblock_create_insert( CntMemblock *mb,
                                      size_t position, size_t count )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_create_insert( MBIMPL_PTR( mb ), position, count );
+    return cnt_memblock_impl_create_insert( MBPIMPL( mb ), position, count );
 }
 
 void *cnt_memblock_delete( CntMemblock *mb, size_t position, size_t count )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_delete( MBIMPL_PTR( mb ), position, count );
+    return cnt_memblock_impl_delete( MBPIMPL( mb ), position, count );
 }
 
 void cnt_memblock_reduce( CntMemblock *mb, size_t count )
 {
     assert( mb != NULL );
-    cnt_memblock_impl_reduce( MBIMPL_PTR( mb ), count );
+    cnt_memblock_impl_reduce( MBPIMPL( mb ), count );
 }
 
 int cnt_memblock_extend( CntMemblock *mb, size_t count )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_resize( MBIMPL_PTR( mb ),
-             cnt_memblock_impl_size( MBIMPL_PTR( mb ) ) + count );
+    return cnt_memblock_impl_resize( MBPIMPL( mb ),
+             cnt_memblock_impl_size( MBPIMPL( mb ) ) + count );
 }
 
 int cnt_memblock_append( CntMemblock *mb, const void *data, size_t len )
 {
     assert( mb != NULL );
-    return cnt_memblock_impl_append( MBIMPL_PTR( mb ), data, len );
+    return cnt_memblock_impl_append( MBPIMPL( mb ), data, len );
 }
 
 
@@ -230,7 +230,7 @@ static void destroy( struct CntObject *obj )
     CntMemblock *container;
 
     CNT_OBJECT_ASSERT_TYPE( obj, cnt_this_object_type_id );
-    container = CNT_OBJECT_CONTAINER( obj, CntMemblock );\
+    container = CNT_OBJECT_CONTAINER( obj, CntMemblock );
 
     cnt_memblock_destroy( container );
 }
