16d5502b444f78a350fee7e938d8935668491223
newenclave <pikratacid@gmail.com>
2014-06-15 15:10:15 +0400

    clone

--- lib/src/cnt-array-impl.h
+++ lib/src/cnt-array-impl.h
@@ -1,6 +1,13 @@
 #ifndef CNT_ARRAY_IMPL_H
 #define CNT_ARRAY_IMPL_H
 
+#include "cnt-memblock-impl.h"
+
+typedef struct CntArrayImpl {
+    CntMemblockImpl        mmblock_;
+    size_t                 element_size_;
+} CntArrayImpl;
 
 
 #endif // CNTARRAYIMPL_H
+
