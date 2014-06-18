20147dd421dc294026f3c45937dcb181d316340d
newenclave <pikratacid@gmail.com>
2014-06-18 13:47:47 +0400

    arr bin insert

--- lib/src/cnt-array-impl.h
+++ lib/src/cnt-array-impl.h
@@ -48,7 +48,7 @@ void cnt_array_impl_swap( CntArrayImpl *larr, CntArrayImpl *rarr );
  * 1 == success
  * 0 == failed
 **/
-int cnt_array_impl_push_back (CntArrayImpl *arr, const void *element );
+int cnt_array_impl_push_back ( CntArrayImpl *arr, const void *element );
 int cnt_array_impl_append (CntArrayImpl *arr, const void *src, size_t count );
 int cnt_array_impl_extend( CntArrayImpl *arr, size_t count );
 
