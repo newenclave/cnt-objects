91a5ad38adbf644f828936a1bcb030b45aaa46ba
newenclave <pikratacid@gmail.com>
2014-06-18 15:29:22 +0400

    heap

--- lib/src/cnt-heap-impl.c
+++ lib/src/cnt-heap-impl.c
@@ -1,2 +1,36 @@
 #include "cnt-heap-impl.h"
 
+#define ARRPTR( hp )        (&(hp)->array_)
+#define ARRALOCATOR( hp )   (ARRPTR( hp )->mblock_.allocator_)
+#define ARRTRAITS( hp )     (ARRPTR( hp )->traits_)
+
+CntHeapImpl *cnt_heap_impl_new( const CntElementTraits *traits,
+                                const CntAllocator *allocator )
+{
+    CntHeapImpl *inst;
+    assert( traits != NULL );
+
+    assert( allocator != NULL );
+    assert( allocator->allocate != NULL );
+    assert( allocator->deallocate != NULL );
+
+    inst = (CntHeapImpl *)allocator->allocate( sizeof(*inst) );
+    if(inst) {
+        cnt_array_impl_init( ARRPTR(inst), traits, allocator );
+    }
+
+    return inst;
+}
+
+void cnt_heap_impl_free( CntHeapImpl *hp )
+{
+    assert( hp != NULL );
+
+    void (*dealloc)(void *) = ARRALOCATOR(hp)->deallocate;
+
+    cnt_array_impl_deinit( ARRPTR(hp) );
+
+    dealloc( hp );
+}
+
+
--- lib/src/cnt-heap-impl.h
+++ lib/src/cnt-heap-impl.h
@@ -1,6 +1,17 @@
 #ifndef CNT_HEAP_IMPL_H
 #define CNT_HEAP_IMPL_H
 
+#include "cnt-array-impl.h"
+
+typedef struct CntHeapImpl {
+    CntArrayImpl    array_;
+} CntHeapImpl;
+
+CntHeapImpl *cnt_heap_impl_new( const CntElementTraits *traits,
+                                const CntAllocator *allocator );
+
+void cnt_heap_impl_free( CntHeapImpl *hp );
 
 
-#endif // CNTHEAPIMPL_H
+#endif // CNT_HEAP_IMPL_H
+
