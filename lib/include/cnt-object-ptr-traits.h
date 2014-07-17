b2903842701513779352f96a4772af6f176b6523
newenclave <pikratacid@gmail.com>
2014-07-17 11:09:21 +0400

    cnt

---
+++ lib/include/cnt-object-ptr-traits.h
@@ -0,0 +1,75 @@
+#ifndef CNT_OBJECT_PTR_TRAITS_H
+#define CNT_OBJECT_PTR_TRAITS_H
+
+#include <string.h>
+#include <assert.h>
+
+#include "cnt-object.h"
+#include "cnt-element-traits.h"
+
+//void (* init)( void *, size_t, size_t );
+//void (* destroy)( void *, size_t );
+//void *(* copy)( void *, const void *, size_t );
+//int  (* compare)( const void *, const void *, size_t );
+//void (* swap)( void *, void *, size_t );
+
+
+static void cnt_opt_init( void *obj_ptr, size_t count, size_t element_size )
+{
+    assert( obj_ptr != NULL );
+    assert( element_size == sizeof( sizeof(CntObject*) ));
+    memset( obj_ptr, count, element_size );
+}
+
+static void  cnt_opt_destroy( void *obj_ptr, size_t element_size )
+{
+    CntObject *obj;
+    assert( obj_ptr != NULL );
+    assert( element_size == sizeof( sizeof(CntObject*) ));
+    obj = *((CntObject **)obj_ptr);
+    cnt_object_decref(obj);
+}
+
+static void *cnt_opt_copy( void *obj_ptr, const void *data, size_t element_size)
+{
+    CntObject *obj;
+    assert( obj_ptr != NULL );
+    assert( element_size == sizeof( sizeof(CntObject*) ));
+    obj = *((CntObject **)obj_ptr);
+
+    cnt_object_incref( obj );
+
+    return obj;
+}
+
+static int cnt_opt_compare( const void *l, const void *r, size_t element_size )
+{
+    CntObject *lobj;
+    CntObject *robj;
+    assert( l != NULL );
+    assert( r != NULL );
+    assert( element_size == sizeof( sizeof(CntObject*) ));
+
+    lobj = *((CntObject **)l);
+    robj = *((CntObject **)r);
+
+    return cnt_objects_compare( lobj, robj );
+}
+
+static void  cnt_opt_swap( void *l, void *r, size_t element_size)
+{
+    CntObject *lobj;
+    CntObject *robj;
+    assert( l != NULL );
+    assert( r != NULL );
+    assert( element_size == sizeof( sizeof(CntObject*) ));
+
+    lobj = *((CntObject **)l);
+    robj = *((CntObject **)r);
+
+    *((CntObject **)r) = lobj;
+    *((CntObject **)l) = robj;
+}
+
+
+#endif // CNTOBJECTPTRTRAITS_H
---
+++ lib/src/cnt-object-ptr-traits.c
@@ -0,0 +1,4 @@
+
+#include "include/cnt-object-ptr-traits.h"
+
+
